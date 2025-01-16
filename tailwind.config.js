700d85a7c1f0bfd8a115f67c2b00d086d94a1f85
Rithik <rithik@example.com>
2025-01-16 17:13:38 +0530

    error resoluved

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,17 +1,16 @@
-const flowbite = require("flowbite-react/tailwind");
+import flowbite from "flowbite-react/tailwind";
 
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
-    flowbite.content(),
+    ...flowbite.content, // Spread the content array
   ],
   theme: {
     extend: {},
   },
   plugins: [
-    flowbite.plugin(),
+    flowbite.plugin, // Use the plugin as a function reference
   ],
-}
-
+};
